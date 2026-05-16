11e1098f86bb26fbcdb58d476ef993f9f97fd3e6
Jon Raphael Apostol <jonraphaelapostol@Jons-MacBook-Pro.local>
2026-05-15 20:04:02 -0700

    About and Job-History pages now display a placeholder

--- jrcapostol-personal-website/app/about/page.tsx
+++ jrcapostol-personal-website/app/about/page.tsx
@@ -1,1 +1,9 @@
 // TODO: Implement about me page
+
+export default function About(){
+    return (
+        <div>
+            <h1>About Me</h1>
+        </div>
+    );
+};
--- jrcapostol-personal-website/app/job-history/page.tsx
+++ jrcapostol-personal-website/app/job-history/page.tsx
@@ -1,1 +1,9 @@
 // TODO: Write job history here
+
+export default function JobHistory(){
+    return (
+        <div>
+            <h1>Job History</h1>
+        </div>
+    );
+};
